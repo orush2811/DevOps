ee32eb2a4b2932dd38779be32c4552fb47043031
orush2811 <oritach2811@gmail.com>
2026-01-06 13:39:24 +0200

    new-code

--- terraform/front-infra-for-cg/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_id" {
-  description = "The S3 bucket ID."
-  value       = aws_s3_bucket.state_bucket
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket         = "ors-bucket-cp-cg-state"
-    key            = "front-infra-for-cg/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -13,14 +6,23 @@ terraform {
     }
   }
 }
-provider "aws"{
-  region = "eu-north-1"
-}
 
-module "front-infra-for-cg" {
-  source = "./front-infra-for-cg"
 
-  region          = var.aws_region
-  owner_tag_value = var.owner_tag_value
-  bucket_name     = var.bucket_name
+module "vpc" {
+  source = "./modules/terraform-aws-vpc"
+
+  name = "my-vpc-1"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
