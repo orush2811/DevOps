5412f2f96628deff36b02ddbc04e3822e20d381b
orush2811 <oritach2811@gmail.com>
2026-01-12 16:09:52 +0200

    AWS VPN GW - working state

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
      aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "6.27.0"
     }
   }
 }
@@ -39,7 +39,7 @@ module "vpc" {
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   enable_vpn_gateway = true
 
 customer_gateways = {
